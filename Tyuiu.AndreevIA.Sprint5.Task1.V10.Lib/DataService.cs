e3db47f9cde79e532a2dea7583552915e2b3bc7a
kreker0001 <krona154@gmail.com>
2025-11-27 20:19:51 +0500

    Task1

--- Tyuiu.AndreevIA.Sprint5.Task1.V10.Lib/DataService.cs
+++ Tyuiu.AndreevIA.Sprint5.Task1.V10.Lib/DataService.cs
@@ -1,7 +1,57 @@
-﻿namespace Tyuiu.AndreevIA.Sprint5.Task1.V10.Lib
-{
-    public class DataService
-    {
+﻿using System.Globalization;
+using tyuiu.cources.programming.interfaces.Sprint5;
 
+namespace Tyuiu.AndreevIA.Sprint5.Task1.V10.Lib
+{
+    public class DataService : ISprint5Task1V10
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            string filePath = Path.GetTempFileName();
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                for (int x = startValue; x <= stopValue; x++)
+                {
+                    double f_x = CalculateFunction(x);
+
+                    string result = FormatResult(f_x);
+
+                    sw.WriteLine(result);
+
+                    Console.WriteLine(result);
+                }
+            }
+
+            return filePath;
+        }
+
+        private double CalculateFunction(int x)
+        {
+            try
+            {
+                double denominator = 2 * x - 1;
+                if (denominator == 0)
+                {
+                    return 0; 
+                }
+
+                double result = ((2 * Math.Cos(x) + 2) / denominator) + Math.Cos(x) - 5 * x + 3;
+                return result;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+
+        private string FormatResult(double value)
+        {
+            if (Math.Abs(value) < 0.01)
+            {
+                return "0";
+            }
+
+            return value.ToString("F2", CultureInfo.InvariantCulture).Replace('.', ',');
+        }
     }
 }
--- Tyuiu.AndreevIA.Sprint5.Task1.V10/Program.cs
+++ Tyuiu.AndreevIA.Sprint5.Task1.V10/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.AndreevIA.Sprint5.Task1.V10.Lib;
+namespace Tyuiu.AndreevIA.Sprint5.Task1.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #1 | Выполнил Андреев И. А. | РППб-25-1";
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Класс File. Запись набора данных в текстовый файл                       *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнил: Андреев Иван Александрович | РППб-25-1                        *");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.SaveToFileTextData();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
